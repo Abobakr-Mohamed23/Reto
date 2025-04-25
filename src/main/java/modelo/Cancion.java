42b12c044569b8046f4061d8a8321008d28fd3a5
DAM1A03 <DAM1A03@B21-14.tic.ish>
2025-04-25 16:07:56 +0200

    Hola

---
+++ src/main/java/modelo/Cancion.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author DAM1A03
+ */
+public class Cancion {
+     int ID_Cancion;
+    String Nombre_Cancion;
+    String Fecha_Lanzamiento;
+    Double Duracion;
+    int ID_Artista;
+
+    public Cancion(int ID_Cancion, String Nombre_Cancion, String Fecha_Lanzamiento, Double Duracion, int ID_Artista) {
+        this.ID_Cancion = ID_Cancion;
+        this.Nombre_Cancion = Nombre_Cancion;
+        this.Fecha_Lanzamiento = Fecha_Lanzamiento;
+        this.Duracion = Duracion;
+        this.ID_Artista = ID_Artista;
+    }
+
+    @Override
+    public String toString() {
+        return "Cancion{" + "ID_Cancion=" + ID_Cancion + ", Nombre_Cancion=" + Nombre_Cancion + ", Fecha_Lanzamiento=" + Fecha_Lanzamiento + ", Duracion=" + Duracion + ", ID_Artista=" + ID_Artista + '}';
+    }
+
+    public int getID_Cancion() {
+        return ID_Cancion;
+    }
+
+    public void setID_Cancion(int ID_Cancion) {
+        this.ID_Cancion = ID_Cancion;
+    }
+
+    public String getNombre_Cancion() {
+        return Nombre_Cancion;
+    }
+
+    public void setNombre_Cancion(String Nombre_Cancion) {
+        this.Nombre_Cancion = Nombre_Cancion;
+    }
+
+    public String getFecha_Lanzamiento() {
+        return Fecha_Lanzamiento;
+    }
+
+    public void setFecha_Lanzamiento(String Fecha_Lanzamiento) {
+        this.Fecha_Lanzamiento = Fecha_Lanzamiento;
+    }
+
+    public Double getDuracion() {
+        return Duracion;
+    }
+
+    public void setDuracion(Double Duracion) {
+        this.Duracion = Duracion;
+    }
+
+    public int getID_Artista() {
+        return ID_Artista;
+    }
+
+    public void setID_Artista(int ID_Artista) {
+        this.ID_Artista = ID_Artista;
+    }
+
+}
---
+++ src/main/java/modelo/Compania_Discografica.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author DAM1A03
+ */
+public class Compania_Discografica {
+    
+    int ID_Compania;
+String Nombre_Compania;
+String Ubicacion;
+String Anio_Fundacion;
+
+    public Compania_Discografica(int ID_Compania, String Nombre_Compania, String Ubicacion, String Anio_Fundacion) {
+        this.ID_Compania = ID_Compania;
+        this.Nombre_Compania = Nombre_Compania;
+        this.Ubicacion = Ubicacion;
+        this.Anio_Fundacion = Anio_Fundacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Compania_Discografica{" + "ID_Compania=" + ID_Compania + ", Nombre_Compania=" + Nombre_Compania + ", Ubicacion=" + Ubicacion + ", Anio_Fundacion=" + Anio_Fundacion + '}';
+    }
+
+    public int getID_Compania() {
+        return ID_Compania;
+    }
+
+    public void setID_Compania(int ID_Compania) {
+        this.ID_Compania = ID_Compania;
+    }
+
+    public String getNombre_Compania() {
+        return Nombre_Compania;
+    }
+
+    public void setNombre_Compania(String Nombre_Compania) {
+        this.Nombre_Compania = Nombre_Compania;
+    }
+
+    public String getUbicacion() {
+        return Ubicacion;
+    }
+
+    public void setUbicacion(String Ubicacion) {
+        this.Ubicacion = Ubicacion;
+    }
+
+    public String getAnio_Fundacion() {
+        return Anio_Fundacion;
+    }
+
+    public void setAnio_Fundacion(String Anio_Fundacion) {
+        this.Anio_Fundacion = Anio_Fundacion;
+    }
+    
+}
