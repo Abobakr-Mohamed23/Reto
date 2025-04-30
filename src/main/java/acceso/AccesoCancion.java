3f04848dc0415578c64e37f261255ef1d10fc7a9
JESSIE1710 <jessicaxhxc17@gmail.com>
2025-04-30 14:35:07 +0200

    calaca accesocancion

--- src/main/java/acceso/AccesoCancion.java
+++ src/main/java/acceso/AccesoCancion.java
@@ -8,6 +8,32 @@ package acceso;
  *
  * @author 34631
  */
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Cancion;
+
 public class AccesoCancion {
     
+    public static void insertar(Cancion cancion)throws ClassNotFoundException, SQLException{
+        
+    }
+    
+    public static List<Cancion> consultarTodos() throws ClassNotFoundException, SQLException{ 
+        return null;
+    }
+    public static Cancion consultar(int codigo) throws ClassNotFoundException, SQLException {
+    return null;
+    }
+      public static boolean modificar(Cancion cancion)
+            throws ClassNotFoundException, SQLException {
+          return false;
+      }
+      public static boolean eliminar(int codigo)
+            throws ClassNotFoundException, SQLException {
+          return false;
+      }
 }
